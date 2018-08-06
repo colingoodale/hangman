6eac6cdef9a68fec66793e94db454c7294b07dd9
Colin <colingoodale@gmail.com>
2018-08-05 20:18:26 -0500

    final changes, absolute garbage

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -2,18 +2,19 @@
 
 
 // Create word Bank
+
 var wordBank = ["Alaska", "Alabama", "Arkansas", "American Samoa", "Arizona", "California", "Colorado", "Connecticut", "District of Columbia", "Delaware", "Florida", "Georgia", "Guam", "Hawaii", "Iowa", "Idaho", "Illinois", "Indiana", "Kansas", "Kentucky", "Louisiana", "Massachusetts", "Maryland", "Maine", "Michigan", "Minnesota", "Missouri", "Mississippi", "Montana", "North Carolina", "North Dakota", "Nebraska", "New Hampshire", "New Jersey", "New Mexico", "Nevada", "New York", "Ohio", "Oklahoma", "Oregon", "Pennsylvania", "Puerto Rico", "Rhode Island", "South Carolina", "South Dakota", "Tennessee", "Texas", "Utah", "Virginia", "Virgin Islands", "Vermont", "Washington", "Wisconsin", "West Virginia", "Wyoming"]
 
 
 var currentWord = wordBank[Math.floor(Math.random() * wordBank.length)];
 
-
-var remainingLetters = currentWord.length;
-
 var answerArray = [];
+
 for (var i = 0; i < currentWord.length; i++) {
     answerArray[i] = "_";
 }
+
+var remainingLetters = currentWord.length;
 
 while (remainingLetters > 0) {
     alert(answerArray.join(" "));
@@ -34,23 +35,7 @@ while (remainingLetters > 0) {
     }
 }
 
-alert(answerArray.join(" "));
+document.getElementById(answerArray.join(" "));
 alert("Good job! The answer was " + currentWord);
 
-
-// Wins (# of times words guessed correctly)
-
-    // Display Words with underline
-    //Reveal Correct letters as user guesses them
-
-//Display number of guesses remaining
-
-//Display letters already guessed
-
-//Game restarts directly after wins/losses
-
-//Bonus
-
-//Play a sound or song when user guesses their correct letter/word
-//Create theme and CSS
 
